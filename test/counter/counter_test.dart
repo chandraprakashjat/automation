931977af4fe56ff990b03e99d10cf02e02424d26
LUX\Ext08472 <prakash.jat@signify.com>
2022-09-11 19:09:22 +0530

    first commit

---
+++ test/counter/counter_test.dart
@@ -0,0 +1,32 @@
+import 'package:automation/counter.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('Counter Class Unit Test', () {
+    test('Initial value', () {
+      expect(Counter().count, 0);
+    });
+
+    test('Increment', () {
+      var counter = Counter();
+
+      counter.increment();
+
+      expect(counter.count, 1);
+    });
+
+    test('Decement Operation', () {
+      var counter = Counter();
+      counter.decrement();
+      expect(counter.count, -1);
+    });
+
+    test('Skip Counter Operation', () {
+      var counter = Counter();
+      counter.decrement();
+      counter.decrement();
+      counter.decrement();
+      expect(counter.count, -2);
+    }, skip: Counter().count == 0 ? 'Skip for zero' : 'Skip');
+  });
+}
